4cf55165f8ab44531fa50989701e7cbeeabb4a53
christina <chrstnaheadley@gmail.com>
2021-01-13 15:23:56 -0500

    adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+products = Product.create(
+  [
+    {
+      name: "t-shirt", price: 20, image_url: "", description: "short sleeve"
+    },
+    {
+      name: "jeans", price: 70, image_url: "https://www.google.com/aclk?sa=l&ai=DChcSEwiZrNqW3JnuAhWs_eMHHXUyDQcYABAPGgJ5bQ&sig=AOD64_2Zjrq2NgLZ_NwtlH6ApAxtthc6qg&adurl&ctype=5&ved=2ahUKEwjkhc2W3JnuAhXBgHIEHcBCC9kQvhd6BQgBEJkB", description: "high rise skinny fit"
+    },
+    {
+      name: "hoodie", price: 50, image_url: "", description: "pull over hoodie"
+    },
+    {
+      name: "baseball cap", price: 25, image_url: "", description: "Actualize logo"
+    }, 
+    {
+      name: "socks", price: 17, image_url: "", description: "bright pink crew"
+    }
+  ]
+)
