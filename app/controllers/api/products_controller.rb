5d7028ca1faef0f87f03eb95967cf59d5f645dd1
christina <chrstnaheadley@gmail.com>
2021-01-19 11:58:38 -0500

    adds restful routes and actions

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,40 @@
 class Api::ProductsController < ApplicationController
+  def index
+    @products = Product.all
+    render "index.json.jb"
+  end
+
+  def show
+    # product_id = params[:id]
+    @product = Product.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+
+  def create
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description]
+    )
+    @product.save
+    render "show.json.jb"
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.image_url = params[:image_url] || @product.image_url
+    @product.description = params[:description] || @product.description
+    @product.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    product = Product.find_by(id: params[:id])
+    product.destroy
+    render json: { message: "Product destroyed successfully!" }
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
+  namespace :api do
+    get "/products" => "products#index"
 
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+    post "/products" => "products#create"
+
+    get "/products/:id" => "products#show"
+
+    patch "/products/:id" => "products#update"
+
+    delete "/products/:id" => "products#destroy"
+  end
+
 end
