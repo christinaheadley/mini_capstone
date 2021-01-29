7107c4f11c61e0b4d6e26111d477bb024951874a
christina <chrstnaheadley@gmail.com>
2021-01-28 22:53:43 -0500

    adds has_many through associations

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,62 +1,45 @@
 class Api::ProductsController < ApplicationController
+  before_action :authenticate_admin, except: [:index, :show]
   def index
-    @products = Product
-    # .title_search(params[:search])
-    .discounted(params[:discount])
-    .sorted(params[:sort], params[:sort_order])
+    @products = Product.all(
+      categories: params[:categories]
+    )
 
-
-    # @products = Product.all
-
-    # if params[:discount]
-    #   @products = @products.where("price < ?", 10)
-    # end
-
-    # if params[:sort] == "price" && params[:sort_order] == "asc"
-    #   @products= @products.order(:price)
-    # elsif params[:sort] == "price" && params[:sort_order] == "desc"
-    #   @products = @products.order(price: :desc)
-    # end
-
-    # if params[:search]
-    #   @products = @products.where("name iLIKE ? or description iLIKE ?", "%#{params[:search]}%")
-    # end
-  
+      # .title_search(params[:search])
+      # .discounted(params[:discount])
+      # .sorted(params[:sort], params[:sort_order])
     render "index.json.jb"
   end
-  
-  def show  
-    # product_id = params[:id]
-    @product = Product.find_by(id: params[:id])
-    render "show.json.jb"
-  end
-  
+
   def create
     @product = Product.new(
       name: params[:name],
       price: params[:price],
-      # image_url: params[:image_url],
       description: params[:description],
       quantity: params[:quantity]
     )
     if @product.save
-      render "show.json.jb" #happy path
+      render "show.json.jb"
     else
-      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity #sad path
+      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
     end
   end
-  
+
+  def show
+    @product = Product.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+
   def update
     @product = Product.find_by(id: params[:id])
     @product.name = params[:name] || @product.name
     @product.price = params[:price] || @product.price
-    # @product.image_url = params[:image_url] || @product.image_url
     @product.description = params[:description] || @product.description
     @product.quantity = params[:quantity] || @product.quantity
     if @product.save
-      render "show.json.jb" #happy path
+      render "show.json.jb"
     else
-      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity #sad path
+      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -45,35 +45,40 @@
 # ]
 # )
 
-images = Image.create(
-  [
-  {
-    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT98r2LJEO31YU0NIVfD1cBJcC4MGwOydzNhBQORvQlyqEcKf7WKGtNVfgWMC5BFAHYwGUIDl8c&usqp=CAc",
-    product_id: 1,
-  },
-  {
-    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbjTsoeu17qwM23ydZ0DmnSQopd6pcSXgjWQ&usqp=CAU",
-    product_id: 2,
-  },
-  {
-    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBKFeOgNytJgdYPr0suyX6X0z-nINS0hzx7Q&usqp=CAU",
-    product_id: 3,
-  },
-  {
-    url: "https://www.baseballsavings.com/wcsstore/CatalogAssetStore/Attachment/images/products/baseball/P148395/f-stlouiscardinals-v.jpg",
-    product_id: 4,
-  },
-  {
-    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTN1WGVOmEs3Nfd-RZba9B0RDxOr6nUGtWOg&usqp=CAU",
-    product_id: 5,
-  },
-  {
-    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrL8UTOGucH_SBkQQ6FPLQCFlnFw4GKLNQWa9f3gBWV1-ZI9UxWo3D1qqQ1R7p6jhnYzheDYQ&usqp=CAc",
-    product_id: 9,
-  },
-  {
-    url: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQxCVJs6wMeKRVBFv4V_ip5CZ__1dMNuNUcWVvw--r65d6tyVNIpEKpaGtxs2gtb3xXi8YYYPdK-fl8&usqp=CAc",
-    product_id: 10,
-  }
-]
-)
+# images = Image.create(
+#   [
+#   {
+#     url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT98r2LJEO31YU0NIVfD1cBJcC4MGwOydzNhBQORvQlyqEcKf7WKGtNVfgWMC5BFAHYwGUIDl8c&usqp=CAc",
+#     product_id: 1,
+#   },
+#   {
+#     url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbjTsoeu17qwM23ydZ0DmnSQopd6pcSXgjWQ&usqp=CAU",
+#     product_id: 2,
+#   },
+#   {
+#     url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBKFeOgNytJgdYPr0suyX6X0z-nINS0hzx7Q&usqp=CAU",
+#     product_id: 3,
+#   },
+#   {
+#     url: "https://www.baseballsavings.com/wcsstore/CatalogAssetStore/Attachment/images/products/baseball/P148395/f-stlouiscardinals-v.jpg",
+#     product_id: 4,
+#   },
+#   {
+#     url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTN1WGVOmEs3Nfd-RZba9B0RDxOr6nUGtWOg&usqp=CAU",
+#     product_id: 5,
+#   },
+#   {
+#     url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrL8UTOGucH_SBkQQ6FPLQCFlnFw4GKLNQWa9f3gBWV1-ZI9UxWo3D1qqQ1R7p6jhnYzheDYQ&usqp=CAc",
+#     product_id: 9,
+#   },
+#   {
+#     url: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQxCVJs6wMeKRVBFv4V_ip5CZ__1dMNuNUcWVvw--r65d6tyVNIpEKpaGtxs2gtb3xXi8YYYPdK-fl8&usqp=CAc"
+#     product_id: 10
+#     {
+
+#     {
+#       url: "https://cdn11.bigcommerce.com/s-jlig51/images/stencil/500x659/products/8588/64148/the-child-back__64882.1604687980.jpg?c=2"
+#     product_id: 10
+#   }
+# ]
+# )
